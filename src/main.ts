b3697c84c9a1a91f0fee2a5576fe5de8afe6957d
shybyte <shybyte@users.noreply.github.com>
2022-05-24 23:00:36 +0200

    simple twgl example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,58 @@
-import './style.css'
+import './style.css';
+import * as twgl from 'twgl.js';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// language=glsl
+const vsSource = `
+  attribute vec4 position;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  void main() {
+    gl_Position = position;
+  }
+`;
+
+// language=glsl
+const fsSource = `
+  precision mediump float;
+
+  uniform vec2 resolution;
+  uniform float time;
+
+  void main() {
+    vec2 uv = gl_FragCoord.xy / resolution;
+    float color = 0.0;
+    // lifted from glslsandbox.com
+    color += sin(uv.x * cos(time / 3.0) * 60.0) + cos(uv.y * cos(time / 2.80) * 10.0);
+    color += sin(uv.y * sin(time / 2.0) * 40.0) + cos(uv.x * sin(time / 1.70) * 40.0);
+    color += sin(uv.x * sin(time / 1.0) * 10.0) + sin(uv.y * sin(time / 3.50) * 80.0);
+    color *= sin(time / 10.0) * 0.5;
+
+    gl_FragColor = vec4(vec3(color * 0.5, sin(color + time / 2.5) * 0.75, color), 1.0);
+  }
+`;
+
+const gl = document.querySelector<HTMLCanvasElement>('#c')!.getContext('webgl')!;
+const programInfo = twgl.createProgramInfo(gl, [vsSource, fsSource]);
+
+const arrays = {
+  position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+};
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+function render(time: number) {
+  twgl.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  const uniforms = {
+    time: time * 0.001,
+    resolution: [gl.canvas.width, gl.canvas.height],
+  };
+
+  gl.useProgram(programInfo.program);
+  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+  twgl.setUniforms(programInfo, uniforms);
+  twgl.drawBufferInfo(gl, bufferInfo);
+
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
