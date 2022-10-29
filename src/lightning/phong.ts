b0cac65b522ed842fc1416de458711f37b9fe299
shybyte <shybyte@users.noreply.github.com>
2022-10-29 02:12:18 +0200

    phong lightning

--- src/lightning/phong.ts
+++ src/lightning/phong.ts
@@ -6,8 +6,9 @@ import {
   renderLoop,
   setProgramAttributeToMyBuffer,
   setupCanvas,
-  repeat,
 } from '../my-utils';
+import { cubeNormalData, cubeVertexData } from './cube';
+import { fragmentShader, vertexShaderSrc } from './shader';
 
 const CUBE_COUNT = 1;
 
@@ -15,131 +16,29 @@ export function main() {
   console.log('Starting main...');
   const { infoDisplayElement, gl, canvas } = setupCanvas();
 
-  // prettier-ignore
-  const vertexDataCube = [
-    // Front
-    0.5, 0.5, 0.5, // top right
-    -.5, 0.5, 0.5, // top left
-    0.5, -.5, 0.5, // bottom right
-    -.5, 0.5, 0.5, // top left
-    -.5, -.5, 0.5, // bottom left
-    0.5, -.5, 0.5, // bottom right
-
-    // Left
-    -.5, 0.5, 0.5,
-    -.5, 0.5, -.5,
-    -.5, -.5, 0.5,
-    -.5, 0.5, -.5,
-    -.5, -.5, -.5,
-    -.5, -.5, 0.5,
-
-    // Back
-    -.5, 0.5, -.5,
-    0.5, 0.5, -.5,
-    -.5, -.5, -.5,
-    0.5, 0.5, -.5,
-    0.5, -.5, -.5,
-    -.5, -.5, -.5,
-
-    0.5, 0.5, -.5,
-    0.5, 0.5, 0.5,
-    0.5, -.5, -.5,
-    0.5, 0.5, 0.5,
-    0.5, -.5, 0.5,
-    0.5, -.5, -0.5,
-
-    // Top
-    0.5, 0.5, 0.5,
-    0.5, 0.5, -.5,
-    -.5, 0.5, 0.5,
-    -.5, 0.5, 0.5,
-    0.5, 0.5, -.5,
-    -.5, 0.5, -.5,
-
-    // Underside
-    0.5, -.5, 0.5,
-    -.5, -.5, 0.5,
-    0.5, -.5, -.5,
-    -.5, -.5, 0.5,
-    -.5, -.5, -.5,
-    0.5, -.5, -.5,
-  ];
-
   const offsetData = [];
+  const cubeCloudSize = 0;
   for (let i = 0; i < CUBE_COUNT; i++) {
-    // offsetData.push((Math.random() - 0.5) * 20, (Math.random() - 0.5) * 20, (Math.random() - 0.5) * 20);
-    offsetData.push(0, 0, 0);
-  }
-
-  const colorDataCube = [];
-  for (let face = 0; face < 6; face++) {
-    let faceColor = randomColor();
-    for (let vertex = 0; vertex < 6; vertex++) {
-      colorDataCube.push(...faceColor);
-    }
+    offsetData.push(
+      (Math.random() - 0.5) * cubeCloudSize,
+      (Math.random() - 0.5) * cubeCloudSize,
+      (Math.random() - 0.5) * cubeCloudSize,
+    );
   }
 
   const colorData = [];
-  for (let i = 0; i < CUBE_COUNT; i++) {
-    colorData.push(...colorDataCube);
+  colorData.push(1, 0, 0);
+  for (let i = 0; i < CUBE_COUNT - 1; i++) {
+    colorData.push(...randomColor());
   }
 
-  const normalData = [
-    ...repeat(6, [0, 0, 1]), // Z+
-    ...repeat(6, [-1, 0, 0]), // X-
-    ...repeat(6, [0, 0, -1]), // Z-
-    ...repeat(6, [1, 0, 0]), // X+
-    ...repeat(6, [0, 1, 0]), // Y+
-    ...repeat(6, [0, -1, 0]), // Y-
-  ];
-  const normalBuffer = createMyBuffer(gl, normalData, 3);
+  const positionBuffer = createMyBuffer(gl, cubeVertexData);
+  const normalBuffer = createMyBuffer(gl, cubeNormalData);
 
-  const positionBuffer = createMyBuffer(gl, vertexDataCube);
   const offsetBuffer = createMyBuffer(gl, offsetData);
   const colorBuffer = createMyBuffer(gl, colorData);
 
-  // language=glsl
-  const program = createProgram(
-    gl,
-    `#version 300 es
-    precision mediump float;
-
-    const vec3 lightDirection = normalize(vec3(0, -2, 2));
-    const float ambient = 0.1;
-
-    in vec3 position;
-    in vec3 color;
-    in vec3 aOffset;
-    in vec3 normal;
-
-    out vec3 vColor;
-    out float vBrightness;
-
-    uniform mat4 matrix;
-    uniform mat4 normalMatrix;
-
-
-    void main() {
-      vColor = color;
-      vec3 worldNormal = (normalMatrix * vec4(normal, 1)).xyz;
-      float diffuse = max(0.0, dot(worldNormal, lightDirection));
-      vBrightness = ambient + diffuse;
-      gl_Position = matrix * vec4(position + aOffset, 1);
-    }
-    `,
-    `#version 300 es
-    precision mediump float;
-
-    in vec3 vColor;
-    in float vBrightness;
-    out vec4 finalColor;
-
-    void main() {
-      finalColor = vec4(vColor * vBrightness, 1);
-    }
-    `,
-  );
-
+  const program = createProgram(gl, vertexShaderSrc, fragmentShader);
   gl.useProgram(program);
   gl.enable(gl.DEPTH_TEST);
 
@@ -149,11 +48,15 @@ export function main() {
   setProgramAttributeToMyBuffer(gl, program, 'normal', normalBuffer);
 
   const offsetLocation = gl.getAttribLocation(program, 'aOffset');
+  const colorLocation = gl.getAttribLocation(program, 'color');
   gl.vertexAttribDivisor(offsetLocation, 1);
+  gl.vertexAttribDivisor(colorLocation, 1);
 
   const uniformLocations = {
     matrix: gl.getUniformLocation(program, 'matrix'),
     normalMatrix: gl.getUniformLocation(program, `normalMatrix`),
+    projection: gl.getUniformLocation(program, 'projection'),
+    modelview: gl.getUniformLocation(program, `modelview`),
   };
 
   const modelMatrix = mat4.create();
@@ -167,7 +70,7 @@ export function main() {
     1e4, // far cull distance
   );
 
-  const viewMatrix = mat4.lookAt(mat4.create(), [0, 0, 3], [0, 0, 0], [0, 1, 0]);
+  const viewMatrix = mat4.lookAt(mat4.create(), [0, 0, 2], [0, 0, 0], [0, 1, 0]);
   const mvMatrix = mat4.create();
   const finalMatrix = mat4.create();
   const normalMatrix = mat4.create();
@@ -181,7 +84,7 @@ export function main() {
 
     mat4.rotateZ(modelMatrix, modelMatrix, 0.6 * deltaTime);
     mat4.rotateX(modelMatrix, modelMatrix, 0.7 * deltaTime);
-    mat4.rotateY(modelMatrix, modelMatrix, 0.7 * deltaTime);
+    mat4.rotateY(modelMatrix, modelMatrix, 0.8 * deltaTime);
 
     mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
 
@@ -189,6 +92,9 @@ export function main() {
     mat4.transpose(normalMatrix, normalMatrix);
 
     mat4.multiply(finalMatrix, projectionMatrix, mvMatrix);
+
+    gl.uniformMatrix4fv(uniformLocations.projection, false, projectionMatrix);
+    gl.uniformMatrix4fv(uniformLocations.modelview, false, mvMatrix);
     gl.uniformMatrix4fv(uniformLocations.matrix, false, finalMatrix);
     gl.uniformMatrix4fv(uniformLocations.normalMatrix, false, normalMatrix);
 
---
+++ src/lightning/shader.ts
@@ -0,0 +1,78 @@
+// language=glsl
+export const vertexShaderSrc = `#version 300 es
+precision mediump float;
+
+const vec3 lightDirection = normalize(vec3(-2, 0, 2));
+const float ambient = 0.1;
+
+in vec3 position;
+in vec3 color;
+in vec3 aOffset;
+in vec3 normal;
+
+out vec3 vColor;
+
+uniform mat4 projection, modelview;
+uniform mat4 matrix;
+uniform mat4 normalMatrix;
+
+out vec3 normalInterp;
+out vec3 vertPos;
+
+void main() {
+  vColor = color;
+  vec4 vertPos4 = modelview * vec4(position + aOffset, 1.0);
+  vertPos = vec3(vertPos4) / vertPos4.w;
+  normalInterp = vec3(normalMatrix * vec4(normal, 0.0));
+  gl_Position = projection * vertPos4;
+}
+`;
+
+// language=glsl
+export const fragmentShader = `#version 300 es
+precision mediump float;
+
+in vec3 vColor;
+in vec3 normalInterp;// Surface normal
+in vec3 vertPos;// Vertex position
+uniform int mode;// Rendering mode
+const float Ka = 1.0;// Ambient reflection coefficient
+const float Kd = 1.0;// Diffuse reflection coefficient
+const float Ks = 1.0;// Specular reflection coefficient
+const float shininessVal = 80.0;// Shininess
+// Material color
+//    const vec3 ambientColor = vec3(0.1, 0.0, 0.0);
+//    const vec3 diffuseColor = vec3(0.80, 1.0, 0.0);
+const vec3 specularColor = vec3(1.0, 1.0, 1.0);
+const vec3 lightPos = vec3(0.0, 10.0, 0.0);// Light position
+
+out vec4 finalColor;
+
+void main() {
+  vec3 ambientColor = 0.1 * vColor;
+  vec3 diffuseColor = vColor;
+  vec3 N = normalize(normalInterp);
+  vec3 L = normalize(lightPos - vertPos);
+
+  // Lambert's cosine law
+  float lambertian = max(dot(N, L), 0.0);
+  float specular = 0.0;
+  if (lambertian > 0.0) {
+    vec3 R = reflect(-L, N);// Reflected light vector
+    vec3 V = normalize(-vertPos);// Vector to viewer
+    // Compute the specular term
+    float specAngle = max(dot(R, V), 0.0);
+    specular = pow(specAngle, shininessVal);
+  }
+  finalColor = vec4(Ka * ambientColor +
+  Kd * lambertian * diffuseColor +
+  Ks * specular * specularColor, 1.0);
+
+  // only ambient
+  if (mode == 2) finalColor = vec4(Ka * ambientColor, 1.0);
+  // only diffuse
+  if (mode == 3) finalColor = vec4(Kd * lambertian * diffuseColor, 1.0);
+  // only specular
+  if (mode == 4) finalColor = vec4(Ks * specular * specularColor, 1.0);
+}
+`;
