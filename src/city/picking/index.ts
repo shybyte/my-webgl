d3ee87cf798d7ff9ac38399ed230026044e1dd33
shybyte <shybyte@users.noreply.github.com>
2022-11-11 11:11:57 +0100

    Extract FrameBuffer from Picker

--- src/city/picking/index.ts
+++ src/city/picking/index.ts
@@ -1,61 +1,13 @@
 import { mat4 } from 'gl-matrix';
+import { FrameBuffer } from '../utils/frame-buffer';
 
 export class Picker {
-  private readonly frameBuffer: WebGLFramebuffer;
+  private readonly frameBuffer: FrameBuffer;
   private readonly pixelData = new Uint8Array(4);
   private readonly singlePixelProjectionMatrix = mat4.create();
 
   constructor(private gl: WebGL2RenderingContext) {
-    // Create a texture to render to
-    const targetTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-    // create a depth renderbuffer
-    const depthBuffer = gl.createRenderbuffer();
-    if (!depthBuffer) {
-      throw new Error('Can not createRenderbuffer');
-    }
-
-    gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
-
-    function setFramebufferAttachmentSizes(width: number, height: number) {
-      gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-      // define size and format of level 0
-      const level = 0;
-      const internalFormat = gl.RGBA;
-      const border = 0;
-      const format = gl.RGBA;
-      const type = gl.UNSIGNED_BYTE;
-      const data = null;
-      gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, data);
-
-      gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
-      gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT24, width, height);
-    }
-
-    // Create and bind the framebuffer
-    const fb = gl.createFramebuffer();
-    if (!fb) {
-      throw new Error('Can not createFramebuffer');
-    }
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-
-    // attach the texture as the first color attachment
-    const attachmentPoint = gl.COLOR_ATTACHMENT0;
-    const level = 0;
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, targetTexture, level);
-
-    // make a depth buffer and the same size as the targetTexture
-    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
-
-    setFramebufferAttachmentSizes(1, 1);
-
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
-    this.frameBuffer = fb;
+    this.frameBuffer = new FrameBuffer(gl, 1, 1);
   }
 
   render(
@@ -65,24 +17,25 @@ export class Picker {
     renderObjects: (singlePixelProjectionMatrix: mat4) => void,
   ): number {
     const gl = this.gl;
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    create1PixelProjectionMatrix(mouseX, mouseY, perspective, gl, this.singlePixelProjectionMatrix);
-    renderObjects(this.singlePixelProjectionMatrix);
+    const id = this.frameBuffer.bind(() => {
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.readPixels(
-      0, // x
-      0, // y
-      1, // width
-      1, // height
-      gl.RGBA, // format
-      gl.UNSIGNED_BYTE, // type
-      this.pixelData,
-    ); // typed array to hold result
-    const id = this.pixelData[0] + (this.pixelData[1] << 8) + (this.pixelData[2] << 16);
+      create1PixelProjectionMatrix(mouseX, mouseY, perspective, gl, this.singlePixelProjectionMatrix);
+      renderObjects(this.singlePixelProjectionMatrix);
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+      gl.readPixels(
+        0, // x
+        0, // y
+        1, // width
+        1, // height
+        gl.RGBA, // format
+        gl.UNSIGNED_BYTE, // type
+        this.pixelData,
+      ); // typed array to hold result
+
+      return this.pixelData[0] + (this.pixelData[1] << 8) + (this.pixelData[2] << 16);
+    });
 
     if (id) {
       return id - 1;
